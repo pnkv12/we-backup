4a3091ad5e5abc9c47abf40d0666949e6e2b1810
pngoc.khanhvy <53236281+pnkv12@users.noreply.github.com>
2022-04-18 05:15:30 +0700

    update cypress file

--- frontend/cypress/integration/ideaDetail.test.js
+++ frontend/cypress/integration/ideaDetail.test.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-const BASE_URL = "http://localhost:3000";
+const baseURL = "https://be-enterprise.herokuapp.com/v1.0";
 const FilterBtn =
   "/html/body/div[1]/div/main/div/div[1]/div[1]/div/div/div[2]/div/div/div";
 const ideaLink1 =
@@ -10,22 +10,15 @@ const ideaLink2 =
 const username = Cypress.env("username");
 const password = Cypress.env("password");
 
-const titleName1 = "Nga vô địch";
-const titleName2 = "Nga vô địch";
+const titleName1 = "Test 1";
+const titleName2 = "Mới chơi Genshin hoặc còn yếu thì chơi nhân vật nào?";
 
 describe("Test existed elements", () => {
   beforeEach(() => {
-    cy.visit(`${BASE_URL}/ideas`);
+    cy.visit(`${baseURL}/ideas`);
     cy.get('input[name="username"]').should("be.visible").type(username);
     cy.get('input[name="password"]').should("be.visible").type(password);
     cy.get('button[type="submit"]').click();
-  });
-
-  it("should show the filter and search box", () => {
-    cy.get('input[name="search"]').should("be.visible").type("trongvip");
-    cy.xpath(FilterBtn).click();
-    cy.xpath('//*[@id="menu-"]/div[3]/ul/li[2]/span[1]/input').click();
-    //npm install -D cypress-xpath
   });
 
   it("should show the first and second idea", () => {
@@ -36,23 +29,23 @@ describe("Test existed elements", () => {
     cy.xpath("//h4").should("have.text", titleName1);
     cy.wait(2000);
     cy.xpath("//a[text()=' Back']").should("be.visible").click();
-    cy.url().should("eq", `${BASE_URL}/ideas`);
+    cy.url().should("eq", `${baseURL}/ideas`);
     cy.get(ideaLink2).should("be.visible");
     cy.get(ideaLink2).click();
     cy.wait(2000);
     cy.xpath("//h4").should("have.text", titleName2);
     cy.wait(2000);
     cy.xpath("//a[text()=' Back']").should("be.visible").click();
-    cy.url().should("eq", `${BASE_URL}/ideas`);
+    cy.url().should("eq", `${baseURL}/ideas`);
   });
 
   it("should test the + New Button and back", () => {
     cy.wait(2000);
     cy.xpath("//a[text()='New']").should("be.visible").click();
-    cy.url().should("eq", `${BASE_URL}/ideas/ideacreate`);
+    cy.url().should("eq", `${baseURL}/ideas/ideacreate`);
     cy.wait(2000);
     cy.xpath("//a[text()=' Back']").should("be.visible").click();
-    cy.url().should("eq", `${BASE_URL}/ideas`);
+    cy.url().should("eq", `${baseURL}/ideas`);
   });
 });
 
@@ -60,7 +53,7 @@ describe("Test idea dynamically", () => {
   let ideaId1;
   let ideaId2;
   beforeEach(() => {
-    cy.visit(`${BASE_URL}/ideas`);
+    cy.visit(`${baseURL}/ideas`);
     cy.get('input[name="username"]').should("be.visible").type(username);
     cy.get('input[name="password"]').should("be.visible").type(password);
     cy.get('button[type="submit"]').click();
@@ -76,24 +69,6 @@ describe("Test idea dynamically", () => {
       .then((href) => {
         ideaId2 = href.substring(7);
       });
-  });
-
-  it("should show the right ideaID", () => {
-    cy.get(ideaLink1).should("be.visible");
-    cy.get(ideaLink1).click();
-
-    cy.wait(1000);
-
-    cy.xpath('//h6[text()="ID: "]').should("have.text", "ID: " + ideaId1);
-  });
-
-  it("should show the right ideaID2", () => {
-    cy.get(ideaLink2).should("be.visible");
-    cy.get(ideaLink2).click();
-
-    cy.wait(1000);
-
-    cy.xpath('//h6[text()="ID: "]').should("have.text", "ID: " + ideaId2);
   });
 
   after(() => {
--- frontend/cypress/integration/login.test.js
+++ frontend/cypress/integration/login.test.js
@@ -1,10 +1,10 @@
 /// <reference types="cypress" />
 
-const BASE_URL = "http://localhost:3000";
+const baseURL = "https://be-enterprise.herokuapp.com/v1.0";
 
 describe("User can log in", () => {
   beforeEach(() => {
-    cy.visit(`${BASE_URL}/login`);
+    cy.visit(`${baseURL}/login`);
     cy.reload();
   });
 
@@ -12,9 +12,9 @@ describe("User can log in", () => {
     cy.get('input[name="username"]').should("be.visible").type(username);
     cy.get('input[name="password"]').should("be.visible").type(password);
     cy.get('button[type="submit"]').click();
-    cy.url().should("eq", `${BASE_URL}/`);
+    cy.url().should("eq", `${baseURL}`);
     cy.get('svg[data-testid="PersonIcon"]').click();
-    cy.xpath('//li//a[text()="Huy"]').should("have.text", "Huy");
+    cy.xpath('//li//a[text()="manager"]').should("have.text", "manager");
   });
 
   it("User logs out successfully", () => {
